6fd8e3bf3d0ab73ace323f54676cbd6a63ef5c43
Asif Momin <asifdmomin@gmail.com>
2025-10-08 14:37:11 +1100

    Add Neovim markdown preview with browser support
    Adds LazyVim markdown extra and custom keybindings for live preview:
    - Space+mp: Open markdown preview in browser
    - Space+mt: Toggle preview on/off
    - Includes init.lua bootstrap for LazyVim setup

---
+++ packages/neovim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview", ft = "markdown" },
+      { "<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview Toggle", ft = "markdown" },
+    },
+  },
+}
